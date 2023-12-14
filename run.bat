5ea4ef806a358c9391b2bfd847a5661ff0350e3d
Happy7 <58347721+Happyuky7@users.noreply.github.com>
2023-12-14 03:16:39 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,11 +8,11 @@ echo  Youtube Downloader Python
 
 echo ㅤ
 
-echo     Created By: @HappyRogelio7
+echo     Created By: @Happyuky7
 
 echo     Version: 1.0.0
 
-echo     Github: https://github.com/HappyRogelio7/YouTube-Downloader-PythonV1
+echo     Github: https://github.com/Happyuky7/YouTube-Downloader-PythonV1
 
 echo ㅤ
 
